0ac0d414fdbd2005ef6ea69bd8e6d35fb3aed1a0
marceau <marceau.tassin@gmail.com>
2020-12-16 16:35:12 +0100

    add sampled_letters to party model

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,10 +1,21 @@
+require 'pry-byebug'
 class PartiesController < ApplicationController
   def new
+    game_init
+    @letters = sampled_letters
+    binding.pry
+    # @party = Party.new
   end
 
   def create
+
   end
 
   def show
+
   end
+
+
 end
+
+
